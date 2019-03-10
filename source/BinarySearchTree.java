9377cb55efbac4adb80cdcd28490b2e414f0726c
Jonah Swain <jonahswain@gmail.com>
2019-03-10 17:51:40 +0200

    [add] Function declarations for BinarySearchTree class

--- source/BinarySearchTree.java
+++ source/BinarySearchTree.java
@@ -24,10 +24,48 @@ public class BinarySearchTree<dataType, keyType extends Comparable<keyType>> {
         this.headNode = null;
     }
 
+    /**
+     * Inserts a new entry into the binary search tree<br>
+     * 
+     * @param key  The key of the entry to insert
+     * @param data  The data to insert
+     */
+    public void insert(keyType key, dataType data){
+        // TODO
+    }
+
+    /**
+     * Gets the data located at the specified key in the tree<br>
+     * 
+     * @param key  The key of the entry to find
+     * @return The data at the specified key (null if key not found)
+     */
+    public dataType get(keyType key){
+        // TODO
+    }
+
+    /**
+     * Returns the contents of the tree as a sorted array (sorted by key)<br>
+     * 
+     * @return The contents of the tree as a sorted array (sorted by key)
+     */
+    public dataType[] toArray(){
+        // TODO
+    }
+
+    /**
+     * Returns the contents of the tree as a string, with each data element on a new line (the data elements must have toString methods)<br>
+     * 
+     * @return The contents of the tree as a string, with each data element seperated by a newline
+     */
+    public String toString(){
+        // TODO
+    }
+
 }
 
 /**
- * <h2>BinarySearchTreeNode</h2><br>
+ * <h2>BinarySearchTreeNode</h2>
  * A node in a binary search tree data structure<br>
  * generic dataType is the (object) type of the data to be stored<br>
  * generic keyType is the (object) type of the key used for the node (must implement the Comparable interface)<br><br>
