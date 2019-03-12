13542674e4b56aa8fe78b51c4d8795788af4db27
Jonah Swain <jonahswain@gmail.com>
2019-03-12 23:44:29 +0200

    [add] Insert and get methods for BinarySearchTree

--- source/BinarySearchTree.java
+++ source/BinarySearchTree.java
@@ -4,6 +4,8 @@ BinarySearchTree
 Author: Jonah Swain
 */
 
+import java.lang.RuntimeException;
+
 /**
  * <h2>BinarySearchTree</h2>
  * A binary search tree data structure<br>
@@ -14,7 +16,7 @@ Author: Jonah Swain
  * @author Jonah Swain (https://github.com/jonahswain/)
  * @since 10/03/2018
  */
-public class BinarySearchTree<dataType, keyType extends Comparable<keyType>> {
+public class BinarySearchTree<dataType, keyType extends Comparable<keyType>>{
 
     /** Head node of the tree */
     private BinarySearchTreeNode<dataType, keyType> headNode;
@@ -34,8 +36,33 @@ public class BinarySearchTree<dataType, keyType extends Comparable<keyType>> {
      * @param key  The key of the entry to insert
      * @param data  The data to insert
      */
-    public void insert(keyType key, dataType data){
-        // TODO
+    public void insert(keyType key, dataType data) throws java.lang.RuntimeException {
+        if (this.headNode == null){ // If head node is null, insert the new node as the head node
+            this.headNode = new BinarySearchTreeNode<dataType, keyType>(key, data);
+        } else { // If the head node is not null, find where to insert the new node
+            BinarySearchTreeNode<dataType, keyType> currentNode = this.headNode;
+            while(true){
+                if (currentNode.key().compareTo(key) > 0){ // Insert to the left
+                    if (currentNode.getLeftChild() == null){ // Insert a child on the current node
+                        currentNode.setLeftChild(new BinarySearchTreeNode<dataType, keyType>(key, data));
+                        break; // Exit loop
+                    } else { // Branch to the next node
+                        currentNode = currentNode.getLeftChild();
+                    }
+                } else if (currentNode.key().compareTo(key) < 0){ // Insert to the right
+                    if (currentNode.getRightChild() == null){ // Insert a child on the current node
+                        currentNode.setRightChild(new BinarySearchTreeNode<dataType, keyType>(key, data));
+                        break; // Exit loop
+                    } else { // Branch to the next node
+                        currentNode = currentNode.getRightChild();
+                    }
+                } else { // Keys are equal
+                    throw new RuntimeException("Duplicate key error"); // Throw an error
+                }
+            }
+        }
+
+        this.treeSize++; // Increment tree size
     }
 
     /**
@@ -45,6 +72,26 @@ public class BinarySearchTree<dataType, keyType extends Comparable<keyType>> {
      * @return The data at the specified key (null if key not found)
      */
     public dataType get(keyType key){
+        BinarySearchTreeNode<dataType, keyType> currentNode = this.headNode;
+        while (currentNode != null){
+            if (currentNode.key().compareTo(key) > 0){ // Search to the left
+                currentNode = currentNode.getLeftChild();
+            } else if (currentNode.key().compareTo(key) < 0){ // Search to the right
+                currentNode = currentNode.getRightChild();
+            } else { // Key is equal (return data)
+                return currentNode.data();
+            }
+        }
+
+        return null; // Key not found, return null
+    }
+
+    /**
+     * Removes the entry in the tree with the specified key<br>
+     * 
+     * @param keyType  The key of the entry to remove
+     */
+    public void delete(keyType key){
         // TODO
     }
 
@@ -111,6 +158,24 @@ class BinarySearchTreeNode<dataType, keyType extends Comparable<keyType>> {
         this.rightChild = null;
         this.key = nodeKey;
         this.data = nodeData;
+    }
+
+    /**
+     * Gets the key of the node
+     * 
+     * @return The key of the node
+     */
+    public keyType key(){
+        return this.key;
+    }
+
+    /**
+     * Gets the data of the node
+     * 
+     * @return The data of the node
+     */
+    public dataType data(){
+        return this.data;
     }
 
     /**
